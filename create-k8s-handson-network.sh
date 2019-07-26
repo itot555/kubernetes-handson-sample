872758a51c4f335207ff52516a72db7c3d1c7304
itot555 <Tadashi.Ito@emc.com>
2019-07-26 18:15:10 +0900

    update

--- create-k8s-handson-network.sh
+++ create-k8s-handson-network.sh
@@ -2,12 +2,16 @@
 gcloud compute networks create k8s-handson \
  --subnet-mode=custom
 
+sleep 60
+
 gcloud compute networks list |grep k8s
 
 gcloud compute networks subnets create k8s-handson-subnet \
  --network=kubernetes-handson \
  --range=192.168.0.0/24 \
  --region=asia-northeast1
+
+sleep 60
 
 gcloud compute networks subnets list |grep k8s
 
@@ -17,5 +21,7 @@ gcloud compute firewall-rules create allow-ssh-steplinux \
  --action allow \
  --target-tags k8s-handson-steplinux \
  --rules tcp:22
+
+sleep 60
 
 gcloud compute firewall-rules list |grep k8s
