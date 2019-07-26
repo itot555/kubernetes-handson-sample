3bf6942421501f5b04b2daea0a3f6d86a84f1bec
tadashi ito <Tadashi.Ito@emc.com>
2019-07-26 17:34:23 +0900

    Create create-k8s-handson-steplinux.sh

---
+++ create-k8s-handson-steplinux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+gcloud compue instances create "steplinux" \
+ --image-family "ubuntu-1804-lts" \
+ --image-project "ubuntu-os-cloud" \
+ --boot-disk-size "30" \
+ --custom-cpu "1" \
+ --custom-memory "2048MB" \
+ --network "k8s-handson" \
+ --subnet "k8s-handson-subnet" \
+ --tags "k8s-handson-steplinux" \
+ --zone asia-northeast1-b \
+ --preemptible
