d8177a29f321b57070c7c2acf2960c8b555e7be4
itot555 <Tadashi.Ito@emc.com>
2019-07-29 23:42:41 +0900

    v0.3

--- delete-k8s-handson-network.sh
+++ delete-k8s-handson-network.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 gcloud compute firewall-rules delete allow-ssh-steplinux
+gcloud compute firewall-rules delete allow-concourse-web
 gcloud compute networks subnets delete k8s-handson-subnet
 gcloud compute networks delete k8s-handson
